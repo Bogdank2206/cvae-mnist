166f08a20f6a8165d32e1905712da69e9ea857cc
Карпузов Богдан Константинович <bkkarpuzov@edu.hse.ru>
2026-07-17 18:14:02 +0300

    connected client and server

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 from app.services.inference import load_model
@@ -16,5 +17,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="CVAE Generator", version="0.1.0", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
